222220e31dcc971c14b1ce6f3d1a7904255b762c
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-08-27 12:52:53 +0200

    task 1 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  static state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Please leave feedback</h1>
+          <div>
+            <button>good</button>
+            <button>neutral</button>
+            <button>bad</button>
+          </div>
+        </div>
+        <div>
+          <h2>Statistics</h2>
+          <p>good</p>
+          <p>neutral</p>
+          <p>neutral</p>
+        </div>
+      </div>
+    );
+  }
+}
